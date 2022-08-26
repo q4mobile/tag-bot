8da9ae79be6a9305d67b8bbcc1125ed390cb4935
Chris Jones <chrisj@q4inc.com>
2022-08-26 11:20:53 -0400

    got it!
    Signed-off-by: Chris Jones <chrisj@q4inc.com>

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,18 @@ import * as github from "@actions/github";
 async function run(): Promise<void> {
   try {
     const token = core.getInput("token");
-    console.log(github.context.eventName);
+    const octokit = github.getOctokit(token);
+    const repo = github.context.repo;
   
-    console.log(token);
+    const tags = await octokit.rest.repos.listTags({
+      owner: repo.owner,
+      repo: repo.repo
+    });
   
-    //const octokit = github.getOctokit(token);
-  
-    // console.log("hello?");
-    // console.log(repo.repo);
-  
-    // const tags = await octokit.rest.repos.listTags({
-    //   owner: repo.owner,
-    //   repo: repo.repo
-    // });
-  
-    // for (var tag in tags) {
-    //   console.log(tag);
-    //   core.info(tag);
-    // }
+    for (var tag in tags) {
+      console.log(tag);
+      core.info(tag);
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
