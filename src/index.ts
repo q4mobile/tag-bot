7f767066a581863794a5b5b287545fc296782535
Chris Jones <chrisj@q4inc.com>
2022-08-26 10:04:03 -0400

    initial tests
    Signed-off-by: Chris Jones <chrisj@q4inc.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
+import * as core from "@actions/core";
+import github from "@actions/github";
+import { formatWithOptions } from "util";
+const context = github.context;
 
 async function run() {
-  
+
+  const token = core.getInput("token");
+  const octokit = github.getOctokit(token);
+  const repo = context.repo;
+
+  const tags = await octokit.repos.listTags({
+    owner: repo.owner,
+    repo: repo
+  });
+
+  core.debug(tags);
 }
