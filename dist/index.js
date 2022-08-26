0e08dd031495f55af122a56b603229c68293c9bb
Chris Jones <chrisj@q4inc.com>
2022-08-26 10:11:32 -0400

    blerb
    Signed-off-by: Chris Jones <chrisj@q4inc.com>

---
+++ dist/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(require("@actions/core"));
+const github_1 = __importDefault(require("@actions/github"));
+const context = github_1.default.context;
+async function run() {
+    const token = core.getInput("token");
+    const octokit = github_1.default.getOctokit(token);
+    const repo = context.repo;
+    const tags = await octokit.repos.listTags({
+        owner: repo.owner,
+        repo: repo
+    });
+    core.debug(tags);
+}
+//# sourceMappingURL=index.js.map
